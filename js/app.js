dcbaaec0c834982561e9c46abbbe684987f44c78
FacundoL <laugledev@gmail.com>
2022-07-15 12:10:48 -0300

    initial commit

---
+++ js/app.js
@@ -0,0 +1,71 @@
+// COMENZANDO CON EL CODIGO. ACA LLAMO A LOS ID QUE TENGO EN EL HTML
+
+const contenidoHeader = document.getElementById('contenidoHeader');
+const contenidoMain = document.getElementById('contenidoMain');
+const carritoContenedor = document.getElementById('carritoContenedor');
+const contenidoFooter = document.getElementById('contenidoFooter');
+
+// ACA CREO DESDE JS UN DIV CON LA CLASE ENLACES, PARA LA UL.
+
+const div = document.createElement('div');
+div.classList.add('enlaces');
+div.innerHTML = `
+<a href="#">Productos</a>
+<a href="#">Contacto</a>`
+
+contenidoHeader.appendChild(div);
+
+// CON UN FOREACH RECORRO STOCKPRODUCTOS Y VOY INSERTANDO LAS DESCRIPCIONES
+
+let carrito = []
+
+stockProductos.forEach((producto) => {
+    const div = document.createElement('div');
+    div.classList.add('producto')
+    div.innerHTML = `
+    <img src=${producto.img}>
+    <h3>${producto.nombre}</h3>
+    <p>${producto.desc}</p>
+    <p>Talle: ${producto.talle}</p>
+    <p class="precioProducto">Precio:$ ${producto.precio}</p>
+    <button id="agregar${producto.id}" class="boton-agregar">Agregar al carrito</button>
+    `
+    contenidoMain.appendChild(div);
+    
+    const boton = document.getElementById(`agregar${producto.id}`);
+
+    boton.addEventListener('click', () => {
+        agregarAlCarrito(producto.id);
+    });
+});
+
+
+const agregarAlCarrito = (prodId) => {
+    const item = stockProductos.find((prod) => prod.id === prodId);
+    carrito.push(item);
+    actualizarCarrito();
+};
+
+const eliminarDelCarrito = (prodId) => {
+    const item = carrito.find((prod) => prod.id === prodId);
+    const indice = carrito.indexOf(item);
+    carrito.splice(indice, 1);
+    actualizarCarrito();
+};
+
+const actualizarCarrito = () => {
+    carritoContenedor.innerHTML = "";
+
+    carrito.forEach((prod) => {
+        const div = document.createElement('div');
+        div.className = ('productoEnCarrito');
+        div.innerHTML = `
+        <p>${prod.nombre}</p>
+        <p>Precio:$ ${prod.precio}</p>
+        <p>Cantidad: <span id="cantidad">${prod.cantidad}</span></p>
+        <button onclick = "eliminarDelCarrito(${prod.id})" class="boton-eliminar"><img src="./icon/borrar-64.png"></button>
+        `
+
+        carritoContenedor.appendChild(div);
+    });
+}
---
+++ js/stock.js
@@ -0,0 +1,11 @@
+let stockProductos = [
+    {id: 1, nombre: 'Remera', cantidad: 1, desc: 'Una remera que re va con vos', precio: 1250, talle: 'L', img: './imagenes/remera.jpg'},
+    {id: 2, nombre: 'Remera', cantidad: 1, desc: 'Una remera que re va con vos', precio: 1470, talle: 'S', img: './imagenes/remera.jpg'},
+    {id: 3, nombre: 'Remera', cantidad: 1, desc: 'Una remera que re va con vos', precio: 1500, talle: 'M', img: './imagenes/remera.jpg'},
+    {id: 4, nombre: 'Pantalon', cantidad: 1, desc: 'Un pantalon que re va con vos', precio: 2250, talle: 'L', img: './imagenes/pantalon.jpg'},
+    {id: 5, nombre: 'Pantalon', cantidad: 1, desc: 'Un pantalon que re va con vos', precio: 2470, talle: 'S', img: './imagenes/pantalon.jpg'},
+    {id: 6, nombre: 'Pantalon', cantidad: 1, desc: 'Un pantalon que re va con vos', precio: 3500, talle: 'M', img: './imagenes/pantalon.jpg'},
+    {id: 7, nombre: 'Campera', cantidad: 1, desc: 'Un pantalon que re va con vos', precio: 7250, talle: 'L', img: './imagenes/buzo.jpg'},
+    {id: 8, nombre: 'Campera', cantidad: 1, desc: 'Un pantalon que re va con vos', precio: 8470, talle: 'S', img: './imagenes/buzo.jpg'},
+    {id: 9, nombre: 'Campera', cantidad: 1, desc: 'Un pantalon que re va con vos', precio: 9500, talle: 'M', img: './imagenes/buzo.jpg'},
+];
